f0709156f0bcdb2a92278576cf4a15d190536d9d
Amir <amir.shaihutd@gmail.com>
2021-10-15 10:26:10 +0300

    <3 done>

--- 3/scaner.py
+++ 3/scaner.py
@@ -1,25 +1,43 @@
 import socket
 import threading
 
+print(threading.active_count())
 ports = []
-for i in range (0, 10000):
+done = []
+for i in range (0, 1000):
     ports.append(0)
+for i in range (0, 1000):
+    done.append(0)
+
 def open (port):
     sock = socket.socket()
+    sock.settimeout(0.5)
     try:
-        sock.connect(('127.0.0.1', port))
+        sock.connect(('www.mail.ru', port))
     except:
-        print(port, 'dose not opened')
         sock.close()
+        done[port] = 1
     else:
         ports[port] = 1
-        print (port, 'opened')
+        done[port] = 1
         sock.close()
 
 
-t = [threading.Thread(target=open, args=[i]) for i in range(2999, 3002)]
+t = [threading.Thread(target=open, args=[i]) for i in range(1, 1000)]
 [t1.start() for t1 in t]
+
+print(threading.active_count())
+a = 0
+while a  < 999:
+    b = 0
+    for i in range(0, 1000):
+    	b += done[i]
+    a = b
+    print (a, "from 999")
+
 [t1.join() for t1 in t]
-for i in range (0, 10000):
-    if ports[i] != 0:
-        print ("Port ", i, "opened")
+for i in range (0, 1000):
+    if ports[i] == 1:
+        print ("Port ", i + 1, "opened")
+    else:
+        print ("Port ", i + 1, "not opened")
